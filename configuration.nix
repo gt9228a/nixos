2f10bc4cda0e0bcccc81fab2ef818a9f8b1bc4f6
gt9228a <gt9228a@gmail.com>
2024-03-20 21:53:14 -0400

    nvidia and libvirt

--- configuration.nix
+++ configuration.nix
@@ -1,58 +1,56 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  
+  boot.kernelModules = [ "vfio_pci" "vfio_iommu_type1" "vfio" "kvmfr"];
+  boot.extraModprobeConfig = ''
+    options vfio-pci ids=1002:67df,1002:aaf0,8086:15b8
+    options kvmfr static_size_mb=64
+  '';
+  
+  boot.kernelParams = [ "intel_iommu=on" "iommu=pt" "pcie_acs_override=downstream,multifunction" "kvm_intel.nested=1" "kvm.ignore_msrs=1" "kvm_intel.emulate_invalid_guest_state=0" ];
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.extraModulePackages = [ pkgs.linuxPackages_zen.kvmfr ];  
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  services.udev.extraRules = ''
+    SUBSYSTEM=="kvmfr", OWNER="michael", GROUP="libvirtd", MODE="0666"
+  '';
+ 
 
-  # Set your time zone.
+  networking.hostName = "nixos"; 
+  networking.networkmanager.enable = true;  
+
   time.timeZone = "America/New_York";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
   };
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-
-  # Enable the Plasma 5 Desktop Environment.
+  services.xserver.videoDrivers = [ "nvidia" ];
   services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
-  
-
-  # Configure keymap in X11
   services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+#  services.xrdp.enable = true;
+#  services.xrdp.defaultWindowManager = "startplasma-x11";
+#  services.xrdp.openFirewall = true;
+#  services.getty.autologinUser = "michael";
+#  services.xserver.displayManager.autoLogin.enable = true;
+#  services.xserver.displayManager.autoLogin.user = "michael";
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+  services.printing.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -62,74 +60,72 @@
     jack.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.michael = {
      isNormalUser = true;
      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
      packages = with pkgs; [
-  #     firefox
-  #     tree
      ];
    };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
    environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     vim 
      git
      wget
      nano
      curl
+     swtpm   
    ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+  virtualisation.libvirtd.qemu.swtpm.enable = true;
+  virtualisation.libvirtd.qemu.ovmf.packages = [pkgs.OVMFFull.fd];
+  virtualisation.libvirtd.qemu.verbatimConfig = ''
+    cgroup_device_acl = [
+    "/dev/kvmfr0", 
+    "/dev/null", "/dev/full", "/dev/zero",
+    "/dev/random", "/dev/urandom",
+    "/dev/ptmx", "/dev/kvm", "/dev/kqemu",
+    "/dev/rtc", "/dev/hpet","/dev/net/tun",
+    "/dev/vfio/vfio",
+        ]
+    '';
+  
+  virtualisation.kvmgt.enable = true;
+  virtualisation.kvmgt.vgpus = {
+  "i915-GVTg_V5_4" = {
+    uuid = [ "dfc6215b-fb46-4755-83b4-7a12175fac5b" ];
+    };
+  };
 
-  # Enable the OpenSSH daemon.
+  environment.etc = {
+  "ovmf/edk2-x86_64-secure-code.fd" = {
+    source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+   };
+
+  "ovmf/edk2-i386-vars.fd" = {
+    source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
+   };
+  };
+
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
+  services.openssh.settings.X11Forwarding = true;
+  services.tailscale.enable = true;
+  
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  networking.firewall.enable = false;
   system.stateVersion = "24.05"; # Did you read the comment?
-
-nixpkgs.config.allowUnfree = true;
-nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 }
 
