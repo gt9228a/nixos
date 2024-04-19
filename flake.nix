6eedd958d8278a65e094f859faa1728c09978b40
gt9228a <gt9228a@users.noreply.github.com>
2024-04-19 17:40:09 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,14 +7,9 @@
       url = "github:nix-community/home-manager/";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-   plasma-manager = {
-      url = "github:pjones/plasma-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
   };
 
-  outputs = inputs@{nixpkgs, home-manager, plasma-manager, ... }: {
+  outputs = inputs@{nixpkgs, home-manager, ... }: {
     nixosConfigurations= {
       nixos= nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -26,8 +21,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.michael = import ./home.nix;
-            home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
-          }
+           }
       ];
     };
   };
